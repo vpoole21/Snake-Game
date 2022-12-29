bd9170efeea5f032162783558f374ed2889bbf95
vpoole21 <121525274+vpoole21@users.noreply.github.com>
2022-12-29 17:07:27 -0600

    Add files via upload

---
+++ scoreboard.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 12, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
+        self.penup()
+        self.color("white")
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.score))
+                self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+
+   #def game_over(self):
+        #self.goto(0, 0)
+        #self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+
+
+
+
